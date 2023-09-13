a8f54f96d36950541ff4bce81a5284b35ffd6ea2
Fogloster <radionssss@gmail.com>
2023-09-13 20:35:49 +0300

    Update loading_bar.js

--- loading_bar.js
+++ loading_bar.js
@@ -7,6 +7,6 @@ CSS.registerProperty({
 // НЕ ЗАБЫТЬ ПОМЕНЯТЬ УРЛ
 // НЕ ЗАБЫТЬ РАСКОМЕНТИТЬ
    setTimeout(function(){
-     window.location.href = 'file:///C:/Users/Radik/Desktop/vtm_project2/black_forumm.html';
+     window.location.href = '';
   }, 5000);
  
