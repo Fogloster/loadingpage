1dad48a0d2fe2869bef0945883fc22b638aa591f
Fogloster <radionssss@gmail.com>
2023-09-13 19:36:47 +0300

    added loading bar page

---
+++ loading_bar.js
@@ -0,0 +1,12 @@
+CSS.registerProperty({
+    name: "--p",
+    syntax: "<integer>",
+    initialValue: 0,
+    inherits: true,
+  }); 
+// НЕ ЗАБЫТЬ ПОМЕНЯТЬ УРЛ
+// НЕ ЗАБЫТЬ РАСКОМЕНТИТЬ
+   setTimeout(function(){
+     window.location.href = 'file:///C:/Users/Radik/Desktop/vtm_project2/black_forumm.html';
+  }, 5000);
+ 
